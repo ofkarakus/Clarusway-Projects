74c8c8a598d523dcc83873c2490d3ee610d1bd1c
ofkarakus <omerfarukkarakus01@gmail.com>
2020-09-17 16:12:26 +0100

    lesson notes hands on

---
+++ javascript/class-notes/15-09-20 data types/myScript.js
@@ -0,0 +1,17 @@
+console.log("hello");
+
+var myNumber = 5;
+
+console.log(myNumber);
+
+const myString = 'merhaba';
+const yourStr = `hi turkish hi ${myString}`;
+console.log('mystring : ' +  myString);
+console.log(yourStr);
+
+const myBool = 5 < 2;
+
+console.log(myBool); 
+
+const mystr = 'Boyle yasamaktan biktim ben usta!';
+console.log(mystr.indexOf('ta'))
